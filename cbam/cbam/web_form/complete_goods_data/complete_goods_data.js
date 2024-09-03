507a278daa574491c88b74b6166d422c149534bc
NDG-Remote <103729772+NDG-Remote@users.noreply.github.com>
2024-09-03 14:30:49 +0000

    Deleted old Web Forms, second try

--- cbam/cbam/web_form/complete_goods_data/complete_goods_data.js
+++ cbam/cbam/web_form/complete_goods_data/complete_goods_data.js
@@ -1,18 +1,21 @@
 frappe.ready(function() {
-	// Corrected after_load function
-	frappe.web_form.after_load = function() {
-		frappe.msgprint("test");  // This should show "test" when the form loads
-
-		// Make sure 'supplier' is a string representing the field name
-		let value = frappe.web_form.get_value('supplier');
-		frappe.msgprint(value); // This will show the value of the supplier field
-	}
+	// frappe.web_form.after_load = () => {
+	// 	frappe.msgprint('Please fill all values carefully');
+	// }
+	// // Corrected after_load function
+	// frappe.web_form.after_load = function() {
+	// 	frappe.msgprint("test");  // This should show "test" when the form loads
+	// 	console.log("Test Console")
+	// 	// Make sure 'supplier' is a string representing the field name
+	// 	let value = frappe.web_form.get_value('supplier');
+	// 	frappe.msgprint(value); // This will show the value of the supplier field
+	// }
 
 	frappe.web_form.validate = function() {
 		let data = frappe.web_form.get_values();
 		if (!data.is_data_confirmed) {
 			frappe.msgprint('Please confirm the data by clicking the checkbox');
-			return false;
+			return;
 		}
 	};
 });
