3b9324a57fce228f14d6725bfdf2df52cd168d9f
Andrea Glaus <andrea.glaus.digital@gmail.com>
2024-09-09 08:04:48 +0000

    Fine-tuning fields and web froms, changed country field in supplier

--- cbam/cbam/web_form/add_employee_details/add_employee_details.py
+++ cbam/cbam/web_form/add_employee_details/add_employee_details.py
@@ -1,9 +1,10 @@
-import frappe
-# Inserting in Web Form still open
+# import frappe
+# # Inserting in Web Form still open
 def get_context(context):
-	user = frappe.session.user
-	supplier_employee_docname = frappe.get_all('Supplier Employee', filters={'email': user}, fields=['name'])
-	if supplier_employee_docname:
-		supplier = frappe.get_value('Supplier Employee', supplier_employee_docname[0].name, 'supplier_company')
-	else:
-		frappe.throw("We couldn't find to which supplier you belong to. Please contact ...")
+# 	user = frappe.session.user
+# 	supplier_employee_docname = frappe.get_all('Supplier Employee', filters={'email': user}, fields=['name'])
+# 	if supplier_employee_docname:
+# 		supplier = frappe.get_value('Supplier Employee', supplier_employee_docname[0].name, 'supplier_company')
+# 	else:
+# 		frappe.throw("We couldn't find to which supplier you belong to. Please contact ...")
+	pass
