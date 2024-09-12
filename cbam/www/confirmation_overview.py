d8eccfbd12c4463fd31c29d2320ea45a75e33fbe
Andrea Glaus <andrea.glaus.digital@gmail.com>
2024-09-12 14:04:13 +0000

    Improved Web Pages

--- cbam/www/confirmation_overview.py
+++ cbam/www/confirmation_overview.py
@@ -4,8 +4,8 @@ no_cache = 1
 def get_context(context):
     #context.test = frappe.get_value('Supplier Employee', "EMP-078-00000007", "supplier_company")
     context.user = frappe.session.user
-    context.employee_list = frappe.db.get_all('Supplier Employee', filters={'email': "lastname@liferant10.us"}, fields=['name'], pluck="name")
-    context.employee = context.employee_list[0]
+    context.employee_list = frappe.db.get_all('Supplier Employee', filters={'email': context.user}, fields=['name'], pluck="name") or []
+    context.employee = context.employee_list[0] or "No employee found"
     context.supplier = frappe.db.get_value('Supplier Employee', {'email': context.user}, 'supplier_company')
     context.supplier_doc = frappe.get_doc('Supplier', context.supplier)
     #context.supplier_doc_dict = context.supplier_doc.as_dict()
