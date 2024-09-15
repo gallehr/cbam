5dbfc68709d40bc494147f672a9bcf28613e605d
Andrea Glaus <andrea.glaus.digital@gmail.com>
2024-09-15 17:28:05 +0000

    Minor Change for other deployement

--- cbam/www/confirmation_overview.py
+++ cbam/www/confirmation_overview.py
@@ -3,7 +3,7 @@ no_cache = 1
 
 def get_context(context):
     context.user = frappe.session.user
-    context.employee_list = frappe.db.get_all('Supplier Employee', filters={'email': context.user}, fields=['name'], pluck="name")
+    context.employee_list = frappe.db.get_all('Supplier Employee', filters={'email': context.user}, fields=['name'], pluck="name") 
     try:
         context.employee = context.employee_list[0]
         context.supplier = frappe.db.get_value('Supplier Employee', {'email': context.user}, 'supplier_company')
