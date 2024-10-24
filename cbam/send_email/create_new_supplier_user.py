cc71c81677df3d5c59fd87a5c04a548a3df22358
Nasir Khan <work.nasirkhan@gmail.com>
2024-10-24 15:41:27 +0000

    feat(82): Automatic sending of Email to Sub-Suppliers and Coworkers of Supplier Employee

--- cbam/send_email/create_new_supplier_user.py
+++ cbam/send_email/create_new_supplier_user.py
@@ -5,24 +5,22 @@ import json
 
 @frappe.whitelist()  # Called by Send Email button through goods.js
 def create_new_supplier_user(employee):
-    frappe.msgprint(f"From create_new_supplier_user: Creating new user for {employee}")
-    employee_email = frappe.db.get_value("Supplier Employee", employee, "email")
-    if not frappe.db.exists("User", employee_email):
-        employee_docname = frappe.db.get_value("Supplier Employee", employee, "name")
-        employee_last_name = frappe.db.get_value("Supplier Employee", employee, "last_name")
-        employee_first_name = frappe.db.get_value("Supplier Employee", employee, "first_name") or employee_last_name
-        
+    #frappe.msgprint(f"From create_new_supplier_user: Creating new user for {employee}")
+    employee_doc = frappe.get_doc("Supplier Employee", employee)
+    if not frappe.db.exists("User", employee_doc.email):
 
-        new_user = frappe.new_doc("User")
-        new_user.email = employee_email
-        new_user.send_welcome_email = 0
-        new_user.last_name = employee_last_name
-        new_user.first_name = employee_first_name
-        
-       
-        new_user.append("roles", {
-            'role': 'Supplier',
-        })
+        try:
+            new_user = frappe.new_doc("User")
+            new_user.email = employee_doc.email
+            new_user.send_welcome_email = False
+            new_user.last_name = employee_doc.last_name
+            new_user.first_name = employee_doc.first_name or employee_doc.last_name
+            new_user.append("roles", {
+                'role': 'Supplier'
+            })
       
-        new_user.save()
-        frappe.db.set_value("Supplier Employee", employee_docname, "Status", "Sent to Supplier Employee")
+            new_user.save(ignore_permissions=True)
+            frappe.db.set_value("Supplier Employee", employee_doc.name, "Status", "Sent to Supplier Employee")
+        except Exception as e:
+            frappe.log_error(frappe.get_traceback(), f"An error occured while creating user for {employee_doc.name}")
+       
